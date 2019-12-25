2960380f3b8eb324f2192e6e00f2e0bda87fe8f0
jobscale <jobscale@users.noreply.github.com>
2019-12-25 16:42:21 +0900

    Updates

---
+++ precache-manifest.53d0acb475c3bab2bc41375f8bb3bc9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "56abdd60ce46287ffe2b",
+    "url": "/fake-notification-web-app/static/css/main.cb247ddd.chunk.css"
+  },
+  {
+    "revision": "56abdd60ce46287ffe2b",
+    "url": "/fake-notification-web-app/static/js/main.56abdd60.chunk.js"
+  },
+  {
+    "revision": "eea6ecef820a7011312a",
+    "url": "/fake-notification-web-app/static/js/1.eea6ecef.chunk.js"
+  },
+  {
+    "revision": "1ab9280f8e0afb63a832",
+    "url": "/fake-notification-web-app/static/js/runtime~main.1ab9280f.js"
+  },
+  {
+    "revision": "09d5a128061d340f7189bcfa6df2f75d",
+    "url": "/fake-notification-web-app/static/media/add-home.09d5a128.png"
+  },
+  {
+    "revision": "e570a7be64453c5bd40b47f410fe830b",
+    "url": "/fake-notification-web-app/static/media/background.e570a7be.jpg"
+  },
+  {
+    "revision": "2854f967e75d6c3466a32b123e5198ad",
+    "url": "/fake-notification-web-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fake-notification-web-app/precache-manifest.e649406fe6db6474151065449cab8726.js"
+  "/fake-notification-web-app/precache-manifest.53d0acb475c3bab2bc41375f8bb3bc9b.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/fake-notification-web-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
